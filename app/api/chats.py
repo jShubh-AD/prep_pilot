85c69d1d12e28056e7d30059cbbf4f59467acf26
jShubh-AD <shubhjain798@gmail.com>
2026-06-27 18:46:50 +0530

    added: steam message and removed query expansion for low TTFT

--- app/api/chats.py
+++ app/api/chats.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import StreamingResponse
 from app.schemas.query import QueryRequest
 from app.core.redis_servcie import  get_or_create_session
 from app.graphs.chats.states import QueryState
 from app.graphs.chats.graph import query_graph
 import time
+import json
+from app.core.redis_servcie import get_redis
+from app.schemas.redis_schemas import Session
 
 query_router = APIRouter()
 
@@ -34,6 +38,29 @@ async def send_query(req: QueryRequest):
         queries=[req.query],
         errors=[]
     )
-    state = await query_graph.ainvoke(state)
+
+    async def event_stream():
+        async for chunk in query_graph.astream(state, stream_mode="messages", version="v2"):
+            if chunk["type"] == "messages":
+                message_chunk, metadata = chunk["data"]
+                if metadata["langgraph_node"] == "generate_response":
+                    content = message_chunk.content
+                    if isinstance(content, list) and content:
+                        text = content[0].get("text", "")
+                        if text:
+                            yield f"data: {json.dumps({'text': text, 'ts': time.time(), 'start': start})}\n\n"
+                    elif isinstance(content, str) and content:
+                        yield f"data: {content}\n\n"
+        redis = get_redis()
+        session_data = await redis.get(session_id)
+        session = Session.model_validate_json(session_data)
+        yield f"""data: {json.dumps({
+            'done': True,
+            'session_id': session_id,
+            'total_time': time.time() - start,
+            'tokens_available': 20000 - session.tokens_used,
+            'tokens_used': session.tokens_used
+            })}\n\n"""
+
     print(f"Total time: {time.time() - start:.2f}s")
-    return {"success": True, "query": req.query ,"data": state["llm_ans"],"session_id": session_id}
+    return StreamingResponse(event_stream() ,media_type="text/event-stream")
--- app/graphs/chats/graph.py
+++ app/graphs/chats/graph.py
@@ -13,10 +13,12 @@ build_query_graph.add_node("generate_response", generate_response)
 
 
 # create edges
-build_query_graph.add_edge(START, "query_expansion")
-build_query_graph.add_edge("query_expansion", "query_embedings")
+# build_query_graph.add_edge(START, "query_expansion")
+# build_query_graph.add_edge("query_expansion", "query_embedings")
+build_query_graph.add_edge(START, "query_embedings")
 build_query_graph.add_edge("query_embedings", "retrive_chunks")
 build_query_graph.add_edge("retrive_chunks","generate_response")
 build_query_graph.add_edge("generate_response", END)
+
 
 query_graph = build_query_graph.compile()
