9621ab034cdbdac0c8092c2f710be5053a7d1423
jShubh-AD <shubhjain798@gmail.com>
2026-06-29 18:44:36 +0530

    added: support of 3.5-falsh and 3 flash models

--- app/core/llm.py
+++ app/core/llm.py
@@ -8,7 +8,8 @@ _MODELS = [
     "gemini-2.5-flash",
     "gemini-2.5-flash-lite", 
     "gemini-3.1-flash-lite",
-    "gemini-3.1-flash",
+    "gemini-3-flash",
+    "gemini-3.5-flash"
 ]
 
 def _make_chain(schema=None):
